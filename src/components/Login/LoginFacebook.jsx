f51f6ae93470e831e0dc39df0343a973dfad18f8
tranduy <tranbaoduy4@gmail.com>
2023-07-02 17:46:21 +0700

    fix fb

--- src/components/Login/LoginFacebook.jsx
+++ src/components/Login/LoginFacebook.jsx
@@ -17,33 +17,65 @@ function LoginFacebook() {
             .then(res => {
                 dispatch(loginSuccess(res.user))
                 navigate('/')
-                toast.update(id, { render: "Đăng nhập thành công", isLoading: false, type: 'success' ,autoClose:1200})
+                toast.update(id, { render: "Đăng nhập thành công", isLoading: false, type: 'success', autoClose: 1200 })
             })
             .catch(err => {
-                toast.update(id, { render: "Đăng nhập không thành công. Vui lòng thử lại", isLoading: false, type: 'warning',autoClose:1200 })
+                toast.update(id, { render: "Đăng nhập không thành công. Vui lòng thử lại", isLoading: false, type: 'warning', autoClose: 1200 })
             })
     }
+
+    const test = () => {
+        window.FB.login(function (response) {
+            if (response.status === 'connected') {
+                const id = toast.loading("Đang lấy thông tin...")
+                const accessToken = response.accessToken
+                apiSocial.loginFacebook({ accessToken })
+                    .then(res => {
+                        dispatch(loginSuccess(res.user))
+                        navigate('/')
+                        toast.update(id, { render: "Đăng nhập thành công", isLoading: false, type: 'success', autoClose: 1200 })
+                    })
+                    .catch(err => {
+                        toast.update(id, { render: "Đăng nhập không thành công. Vui lòng thử lại", isLoading: false, type: 'warning', autoClose: 1200 })
+                    })
+            } else {
+                // The person is not logged into your webpage or we are unable to tell. 
+            }
+        });
+    }
     return (
-        <FacebookLogin
-            appId="924184595294405"
-            onSuccess={handleFacebookLogin}
-            onFail={(error) => {
-                
-            }}
-            useCustomerChat={true}
-            // onProfileSuccess={(response) => {
-            //     console.log('Get Profile Success!', response);
-            // }}
-            render={({ onClick, logout }) => (
+        <>
+            <div class="fb-login-button"
+                onClick={test}
+                onlogin="checkLoginState();">
                 <FacebookRoundedIcon
-                    onClick={onClick}
                     sx={{
                         cursor: 'pointer',
                         color: "#4267b2",
                         fontSize: "4.5rem"
-                    }} />
-            )}
-        />
+                    }}></FacebookRoundedIcon>
+            </div>
+            {/* // <FacebookLogin
+        //     appId="924184595294405"
+        //     onSuccess={handleFacebookLogin}
+        //     onFail={(error) => {
+                
+        //     }}
+        //     useCustomerChat={true}
+        //     // onProfileSuccess={(response) => {
+        //     //     console.log('Get Profile Success!', response);
+        //     // }}
+        //     render={({ onClick, logout }) => (
+        //         <FacebookRoundedIcon
+        //             onClick={onClick}
+        //             sx={{
+        //                 cursor: 'pointer',
+        //                 color: "#4267b2",
+        //                 fontSize: "4.5rem"
+        //             }} />
+        //     )}
+        // /> */}
+        </>
 
     )
 }
